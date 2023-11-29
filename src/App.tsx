a1b04cc9571a66261fe085c3e3ab89e2d0887aea
VirAlex <alexou73@gmail.com>
2023-11-29 12:32:53 +0100

    feat: test IS

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
-function App() {
+import algoliasearch from 'algoliasearch/lite'
+import {
+  InstantSearch,
+  SearchBox,
+  useHits,
+  UseHitsProps,
+} from 'react-instantsearch'
+import { BaseHit } from 'instantsearch.js/es/types/results'
+
+// To test Hits
+function CustomHits(props: UseHitsProps<BaseHit> | undefined) {
+  const { hits } = useHits(props)
+  console.log(hits)
+
   return (
-    <>
+    <div>
+      {hits.map((hit: Hit<BaseHit>) => (
+        <div key={hit.objectID}>
+          <p>{hit.name}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+// To test replace with our own
+function App() {
+  const searchClient = algoliasearch(
+    'Q6N17K5UHW',
+    '5b40da503669ba0a30f39a990ff80a65'
+  )
+
+  return (
+    <InstantSearch searchClient={searchClient} indexName="electronics_ns">
       <p>Hello Mate Team</p>
-    </>
+      <SearchBox />
+      <CustomHits />
+    </InstantSearch>
   )
 }
 
