993d64534c33b8d1a205e95679c9d779f83f2eb2
Hugo Valla <Hugo_valla@hotmail.fr>
2023-11-29 22:16:31 +0100

    rmv z-index on RefinementsSelector

--- src/components/facets/refinementsComponents/RefinementsSelector.tsx
+++ src/components/facets/refinementsComponents/RefinementsSelector.tsx
@@ -6,23 +6,30 @@ import { useState } from 'react'
 import { ChevronDown, ChevronUp } from 'react-feather'
 import { UseRefinementListProps, useRefinementList } from 'react-instantsearch'
 
-const RefinementsSelector = ({ label, jsCode, ...props }: UseRefinementListProps & { label: string; jsCode: string }) => {
+const RefinementsSelector = ({
+  label,
+  jsCode,
+  ...props
+}: UseRefinementListProps & { label: string; jsCode: string }) => {
   const { items, refine } = useRefinementList(props)
 
   const handleItemClick = (value: string) => {
     refine(value)
   }
 
-  const { ref, setIsComponentVisible, isComponentVisible } = useComponentVisible(false)
+  const { ref, setIsComponentVisible, isComponentVisible } =
+    useComponentVisible(false)
 
   return (
     <div
-      className="relative z-20 items-start w-1/5 p-2 border border-gray-200 rounded cursor-pointer"
+      className="relative items-start w-1/5 p-2 border border-gray-200 rounded cursor-pointer"
       onClick={() => setIsComponentVisible(!isComponentVisible)}
       ref={ref}
     >
       <div className="flex justify-between">
-        <p className="text-base font-medium text-colorBp-refinementBadgeTextColor">{label}</p>
+        <p className="text-base font-medium text-colorBp-refinementBadgeTextColor">
+          {label}
+        </p>
         <div className="flex items-center gap-2">
           <CodeRevealButton jsCode={jsCode} />
           {isComponentVisible ? <ChevronUp /> : <ChevronDown />}
@@ -37,7 +44,9 @@ const RefinementsSelector = ({ label, jsCode, ...props }: UseRefinementListProps
           <p
             key={item.label}
             onClick={() => handleItemClick(item.value)}
-            className={`${item?.isRefined ? 'bg-black text-white' : 'hover:bg-gray-200'} p-2`}
+            className={`${
+              item?.isRefined ? 'bg-black text-white' : 'hover:bg-gray-200'
+            } p-2`}
           >
             {item.label}
           </p>
