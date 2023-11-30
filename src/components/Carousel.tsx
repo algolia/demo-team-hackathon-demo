931b65ca21eb85cd1767455f0b4b900916e99939
Hugo Valla <Hugo_valla@hotmail.fr>
2023-11-30 11:37:04 +0100

    trending carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -2,10 +2,24 @@ import { Configure, Index, useHits } from 'react-instantsearch'
 import { Hit } from './Hits'
 import { HorizontalSlider } from '@algolia/ui-components-horizontal-slider-react'
 import '@algolia/ui-components-horizontal-slider-theme'
+import CodeRevealButton from './CodeRevealButton'
 
 interface HitProps {
   item: any
 }
+
+const carouselCode = `function App() {
+  // ...
+  return (
+      <TrendingItems
+        recommendClient={recommendClient}
+        indexName={indexName}
+        itemComponent={TrendingItem}
+        facetName={facetName}
+        facetValue={facetValue}
+    />
+  );
+}`
 
 const CarouselHit = ({ item }: HitProps) => {
   return <Hit hit={item} />
@@ -20,10 +34,17 @@ const CarouselHits = () => {
 const Carousel = () => {
   return (
     <Index indexName={'mate_team_off_white_ns'} indexId={'recommend-carousel'}>
-      <Configure query="" hitsPerPage={8} />
+      <Configure
+        query=""
+        hitsPerPage={8}
+        ruleContexts={['trending-carousel']}
+      />
       <div className="bg-colorBp-100 px-3 rounded-md my-4 mb-0 pb-4 pt-2.5 md:px-4">
-        <div className="mx-auto w-full max-w-screen-2xl">
-          <h2 className="mb-2 text-center text-sm font-semibold leading-none text-[#000034] lg:text-base">Recommended for you</h2>
+        <CodeRevealButton jsCode={carouselCode} openDirection="right" />
+        <div className="w-full mx-auto max-w-screen-2xl">
+          <h2 className="mb-2 text-center text-sm font-semibold leading-none text-[#000034] lg:text-base">
+            Trending Products
+          </h2>
           <CarouselHits />
         </div>
       </div>
